39352b0c7850e5838ad534b1c74d6ad024436b0e
Tomas Kriska <kriskatomas360@gmail.com>
2017-06-12 22:13:44 +0200

    added navbar component

---
+++ Gnome.Web/ViewComponets/NavBar.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Gnome.Web.ViewComponets
+{
+    public class NavBar : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(new User());
+        }
+    }
+
+    public class User
+    {
+        public bool IsAuthenticated { get; set; }
+    }
+}
