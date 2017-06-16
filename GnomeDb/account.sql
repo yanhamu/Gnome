06178b5962104fe5e2a89a3546416a5fdf615680
Tomas Kriska <kriskatomas360@gmail.com>
2017-06-17 00:13:58 +0200

    added db project

---
+++ GnomeDb/account.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[account]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	userid int not null,
+	[name] nvarchar(30),
+	[token] nchar(64),
+	foreign key (userid) references [user](id)
+)
---
+++ GnomeDb/user.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[user]
+(
+	[Id] INT NOT NULL identity(1,1) PRIMARY KEY,
+	email varchar(30) not null,
+	[pwd] binary(32) not null,
+	[salt] binary(16) not null
+)
