1afdf19c16d1f1402730a5c5933ab58e7732c2f7
Tomas Kriska <tomas.kriska@actum.cz>
2017-07-06 00:16:03 +0200

    added category repository

---
+++ Gnome.Core.DataAccess/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Gnome.Core.DataAccess
+{
+    public class CategoryRepository
+    {
+        private readonly GnomeDb context;
+
+        public CategoryRepository(GnomeDb context)
+        {
+            this.context = context;
+        }
+
+
+    }
+}
