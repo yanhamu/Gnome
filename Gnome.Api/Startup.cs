ff29b731aa894196bef48176b1269e78115292ea
Tomas Kriska <tomas.kriska@actum.cz>
2017-07-09 20:23:18 +0200

    fixed config files

--- Gnome.Api/Startup.cs
+++ Gnome.Api/Startup.cs
@@ -2,7 +2,6 @@
 using Gnome.Api.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
