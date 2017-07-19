c096453c7403aef5f583decafe91c86a1bf0db03
Tomas Kriska <kriskatomas360@gmail.com>
2017-07-19 22:09:42 +0200

    added local storage for token, implemented accounts data retrieval

--- Gnome.Api/wwwroot/app/account/accounts.js
+++ Gnome.Api/wwwroot/app/account/accounts.js
@@ -1,3 +1,17 @@
 const Accounts = Vue.component('accounts', {
-  template: `<p>account</p>`
-})
+    template: `<p>account</p>`,
+    created: function () {
+        var token = 'bearer ' + store.getToken();
+        var options = { headers: { Authorization: token } };
+        this.$http.get('http://localhost:9020/api/accounts', options)
+            .then(res => {
+                console.log(res.body.result);
+
+            }, res => {
+                console.log('error');
+            });
+    },
+    data: function () {
+        return { accounts: [] }
+    }
+});
--- Gnome.Api/wwwroot/app/auth.js
+++ Gnome.Api/wwwroot/app/auth.js
@@ -7,6 +7,7 @@ store = {
         if (token != null) {
             this.state.isAuthetnicated = true;
             this.state.token = token
+            localStorage.setItem('t', token);
         } else {
             this.state.isAuthetnicated = false;
             this.state.token = null
@@ -15,4 +16,9 @@ store = {
     getToken: function () {
         return this.state.token
     }
+}
+
+var t = localStorage.getItem('t')
+if (t != null) {
+    store.setToken(t);
 }
