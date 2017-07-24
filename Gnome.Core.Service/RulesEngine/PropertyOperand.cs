8245939dbe42c4bfa6e34e64aacfc1c65a3f2b22
Tomas Kriska <kriskatomas360@gmail.com>
2017-07-25 01:22:37 +0200

    decoration

---
+++ Gnome.Core.Service/RulesEngine/PropertyOperand.cs
@@ -0,0 +1,20 @@
+﻿using Gnome.Core.Service.Transactions;
+using System;
+
+namespace Gnome.Core.Service.RulesEngine
+{
+    public class PropertyOperand<TPropertyType> : IOperand<string>
+    {
+        private readonly Func<TransactionRow, TPropertyType> selector;
+
+        public PropertyOperand(Func<TransactionRow, TPropertyType> selector)
+        {
+            this.selector = selector;
+        }
+
+        public string GetValue(TransactionRow transaction)
+        {
+            return selector(transaction).ToString();
+        }
+    }
+}
