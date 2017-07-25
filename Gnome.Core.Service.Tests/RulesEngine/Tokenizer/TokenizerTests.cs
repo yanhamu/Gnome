1cb60eaae8e53e6382e1688b445a7160cca7544d
Tomas Kriska <tomas.kriska@actum.cz>
2017-07-25 13:38:37 +0200

    added tokenizer

---
+++ Gnome.Core.Service.Tests/RulesEngine/Tokenizer/TokenizerTests.cs
@@ -0,0 +1,26 @@
+﻿using Gnome.Core.Service.RulesEngine.Tokenizer;
+using Xunit;
+
+namespace Gnome.Core.Service.Tests.RulesEngine.Tokenizer
+{
+    public class TokenizerTests
+    {
+        [Fact]
+        public void Should_Return_Tokens()
+        {
+            var e = "comment contains 'hello world'";
+            var tokenizer = new Gnome.Core.Service.RulesEngine.Tokenizer.Tokenizer();
+            var tokens = tokenizer.Tokenize(e);
+            
+            AssertToken<OperandToken>(tokens[0], "comment");
+            AssertToken<OperatorToken>(tokens[1], "contains");
+            AssertToken<StringConstantOperandToken>(tokens[2], "hello world");
+        }
+
+        private void AssertToken<T>(IToken token, string expexted)
+        {
+            Assert.IsType<T>(token);
+            Assert.Equal(expexted, token.Value);
+        }
+    }
+}
--- Gnome.Core.Service/RulesEngine/Tokenizer/Tokenizer.cs
+++ Gnome.Core.Service/RulesEngine/Tokenizer/Tokenizer.cs
@@ -5,9 +5,42 @@ namespace Gnome.Core.Service.RulesEngine.Tokenizer
 {
     public class Tokenizer
     {
+        bool lookForSpace = true;
+        bool lookForApostrophe = false;
+
         public List<IToken> Tokenize(string stringExpression)
         {
+            var tokens = new List<IToken>();
+
+            //"comment contains 'hello world'"
+            var trimmed = stringExpression.Trim();
+
+            int fromIndex = 0;
+            int toIndex = GetIndexOfNextToken(fromIndex, trimmed);
+            tokens.Add(GetToken(trimmed.Substring(fromIndex, toIndex - fromIndex)));
+            return tokens;
+        }
+
+        private IToken GetToken(string v)
+        {
             throw new NotImplementedException();
+        }
+
+        private int GetIndexOfNextToken(int fromIndex, string stringExpression)
+        {
+            if (lookForSpace)
+                return stringExpression.IndexOf(' ', fromIndex);
+            if (lookForApostrophe)
+                return GetNextIndexOfApostrophe(fromIndex, stringExpression);
+            throw new Exception("parsing exception");
+        }
+
+        private int GetNextIndexOfApostrophe(int fromIndex, string expression)
+        {
+            var next = expression.IndexOf('\'', fromIndex);
+            if (expression.Length > next && expression[next + 1] == '\'')
+                return GetNextIndexOfApostrophe(next + 1, expression);
+            return next;
         }
     }
 }
