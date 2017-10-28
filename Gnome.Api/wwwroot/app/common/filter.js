b0dbc5a94c9975f9b831c26e834b61e97b82eb8e
Tomas Kriska <kriskatomas360@gmail.com>
2017-10-28 21:37:48 +0200

    added universal filter

---
+++ Gnome.Api/wwwroot/app/common/filter.js
@@ -0,0 +1,35 @@
+﻿const DateFilter = Vue.component('date-filter', {
+    props: ['ranges', 'dates'],
+    created: function () {
+        //this.dates.fromDate = moment().subtract(1, 'months').format('YYYY-MM-DD');
+        //this.dates.toDate = moment().format('YYYY-MM-DD');
+    },
+    mounted: function () {
+        var self = this;
+        $('#fromDateInput').daterangepicker({
+            ranges: self.ranges
+        });
+
+        $('#fromDateInput').on('apply.daterangepicker', function (ev, picker) {
+            self.dates.fromDate = picker.startDate.format('YYYY-MM-DD');
+            self.dates.toDate = picker.endDate.format('YYYY-MM-DD');
+        });
+    },
+    data: function () {
+        return {
+            fromDate: null,
+            toDate: null,
+        }
+    },
+    methods: {
+    },
+    template: `
+<div class="container-fluid">
+    <div class="form-group row">
+        <label for="name" class="col-sm-2 col-form-label">From</label>
+        <div class="col-sm-4">
+            <input id="fromDateInput" class="form-control"/>
+        </div>
+    </div>
+</div>`
+});
--- Gnome.Api/wwwroot/app/reports/report-view.js
+++ Gnome.Api/wwwroot/app/reports/report-view.js
@@ -1,40 +1,53 @@
 ﻿const ReportView = Vue.component('report-view', {
     data: function () {
         return {
-            data: null
+            data: null,
+            ranges: {
+                'This Month': [moment().startOf('month'), moment()],
+                'Last Month': [moment().startOf('month').subtract(1, 'month'), moment()],
+                'Last 3 Months': [moment().startOf('month').subtract(3, 'month'), moment()],
+                'Last 6 Months': [moment().startOf('month').subtract(6, 'month'), moment()],
+                'Last Year': [moment().startOf('month').subtract(1, 'year'), moment()]
+            },
+            dates: {
+                fromDate: moment().subtract(1, 'months').format('YYYY-MM-DD'),
+                toDate: moment().format('YYYY-MM-DD')
+            }
         };
     },
     props: ['id'],
     created: function () {
+
     },
     methods: {
         get: function () {
+
             this.$http
-                .get('reports/' + this.id + '?from=2017-01-01&to=2017-12-12')
+                .get('reports/' + this.id + '?from=' + this.dates.fromDate + '&to=' + this.dates.toDate)
                 .then(res => this.data = res.body);
         }
     },
     template: `
-  <div class="container-fluid">
+<div class="container-fluid">
     <h3>report view</h3>
-filter
+    <date-filter v-bind:ranges="ranges" v-bind:dates="dates" />
 
-<div class="row">
-    <div class="col-sm-12">
-        <button class="btn btn-primary" v-on:click="get">get</button>
+    <div class="row">
+        <div class="col-sm-12">
+            <button class="btn btn-primary" v-on:click="get">get</button>
+        </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-sm-12">
-        <table class="table table-hover table-bordered" v-if="data">
-            <tbody>
-                <tr v-for="d in data.aggregates">
-                    <td>{{d.interval.from|formatDate}} - {{d.interval.to|formatDate}}</td>
-                    <td>{{d.expences}}</td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="row">
+        <div class="col-sm-6">
+            <table class="table table-hover table-bordered" v-if="data">
+                <tbody>
+                    <tr v-for="d in data.aggregates">
+                        <td>{{d.interval.from|formatDate}} - {{d.interval.to|formatDate}}</td>
+                        <td>{{d.expences}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
-  </div>`
+</div>`
 })
