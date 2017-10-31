bc8bb92fef5bd955b6595017fbb2f21a05b8e462
Tomas Kriska <tomas.kriska@actum.cz>
2017-10-31 14:43:32 +0100

    added category tests

--- Gnome.Api.IntegrationTests/CategoryControllerTests.cs
+++ Gnome.Api.IntegrationTests/CategoryControllerTests.cs
@@ -1,6 +1,8 @@
 ﻿using Gnome.Api.IntegrationTests.Extensions;
 using Gnome.Api.IntegrationTests.Fixtures;
+using Gnome.Api.Services.Categories.Requests;
 using Gnome.Core.Model.Database;
+using System;
 using System.Net;
 using Xunit;
 
@@ -29,6 +31,31 @@ namespace Gnome.Api.IntegrationTests
             this.server.PrepareUser(UserFixture.User);
             this.server.PrepareCategory(CategoryFixture.Root);
             var response = await client.List();
+            response.HasStatusCode(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async void Should_Update_Category()
+        {
+            this.server.PrepareUser(UserFixture.User);
+            this.server.PrepareCategory(CategoryFixture.Root);
+            var toUpdate = CategoryFixture.Root;
+            toUpdate.Name = "updated";
+
+            var response = await client.Update(CategoryFixture.Root.Id, toUpdate);
+
+            response.HasStatusCode(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async void Should_Create_Category()
+        {
+            this.server.PrepareUser(UserFixture.User);
+            this.server.PrepareCategory(CategoryFixture.Root);
+            var toCreate = new CreateCategory(CategoryFixture.Root.Id, "new", default(Guid));
+
+            var response = await client.Create(toCreate);
+
             response.HasStatusCode(HttpStatusCode.OK);
         }
     }
