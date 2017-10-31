4a07288cc3ad29fa4f98c6149fbd673198d5fd32
Tomas Kriska <tomas.kriska@actum.cz>
2017-10-31 18:08:02 +0100

    Added user service tests

--- Gnome.Core.Service.Tests/UserServiceTests.cs
+++ Gnome.Core.Service.Tests/UserServiceTests.cs
@@ -1,4 +1,9 @@
-﻿using Xunit;
+﻿using Gnome.Core.DataAccess;
+using Gnome.Core.Model.Database;
+using Gnome.Core.Service.Interfaces;
+using NSubstitute;
+using System;
+using Xunit;
 
 namespace Gnome.Core.Service.Tests
 {
@@ -7,7 +12,59 @@ namespace Gnome.Core.Service.Tests
         [Fact]
         public void Should_Verify_Existing_User()
         {
-            //var service = new UserService(userRepository, securityRepository, securityService);
+            var securityRepository = Substitute.For<IUserSecurityRepository>();
+            var securityService = Substitute.For<IUserSecurityService>();
+
+            securityRepository.GetBy(Arg.Any<string>()).Returns(new Model.Database.UserSecurity());
+            securityService.Verify(Arg.Any<string>(), Arg.Any<byte[]>(), Arg.Any<byte[]>()).Returns(true);
+
+            var service = new UserService(null, securityRepository, securityService);
+            var user = service.Verify("existing@user.email", "password");
+            Assert.NotNull(user);
+        }
+
+        [Fact]
+        public void Should_Verify_Existing_User_With_Wrong_Password()
+        {
+            var securityRepository = Substitute.For<IUserSecurityRepository>();
+            var securityService = Substitute.For<IUserSecurityService>();
+
+            securityRepository.GetBy(Arg.Any<string>()).Returns(new UserSecurity());
+            securityService.Verify(Arg.Any<string>(), Arg.Any<byte[]>(), Arg.Any<byte[]>()).Returns(false);
+
+            var service = new UserService(null, securityRepository, securityService);
+            var user = service.Verify("existing@user.email", "password");
+            Assert.Null(user);
+        }
+
+        [Fact]
+        public void Should_Verify_NonExisting_User()
+        {
+            var securityRepository = Substitute.For<IUserSecurityRepository>();
+
+            securityRepository.GetBy(Arg.Any<string>()).Returns(default(UserSecurity));
+
+            var service = new UserService(null, securityRepository, null);
+            var user = service.Verify("existing@user.email", "password");
+            Assert.Null(user);
+        }
+
+        [Fact]
+        public void Should_Fail_To_Throw_Exception()
+        {
+            var securityRepository = Substitute.For<IUserSecurityRepository>();
+            var securityService = Substitute.For<IUserSecurityService>();
+
+            securityRepository.CreateNew(
+                Arg.Any<string>(),
+                Arg.Any<byte[]>(),
+                Arg.Any<byte[]>(),
+                default(Guid)).Returns(default(Gnome.Core.Model.Database.UserSecurity));
+
+            var userService = new UserService(null, securityRepository, securityService);
+
+            Assert.Throws<InvalidOperationException>(() => userService.CreateNew(null, null, default(Guid)));
+
         }
     }
 }
