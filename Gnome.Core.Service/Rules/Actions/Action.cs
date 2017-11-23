656d93dc8ad3dfdf7eb85e998b409f4ddcbb4132
Tomas Kriska <tomas.kriska@actum.cz>
2017-11-23 17:04:19 +0100

    added actions

---
+++ Gnome.Core.Service/Rules/Actions/Action.cs
@@ -0,0 +1,25 @@
+﻿using Gnome.Core.Model.Database;
+using Gnome.Core.Service.Transactions;
+
+namespace Gnome.Core.Service.Rules.Actions
+{
+    public abstract class Action
+    {
+        public string Type { get; }
+
+        public bool IsSuitable(string type)
+        {
+            return this.Type == type;
+        }
+
+        public Action(Rule rule)
+        {
+            this.Type = rule.ActionType;
+            this.Initialize(rule.ActionData);
+        }
+
+        protected abstract void Initialize(string actionData);
+        protected abstract string GetData();
+        public abstract void Process(TransactionCategoryRow transaction);
+    }
+}
---
+++ Gnome.Core.Service/Rules/Actions/AssignCategory.cs
@@ -0,0 +1,29 @@
+﻿using Gnome.Core.Model.Database;
+using Gnome.Core.Service.Transactions;
+using Newtonsoft.Json;
+using System;
+
+namespace Gnome.Core.Service.Rules.Actions
+{
+    public class AssignCategory : Action
+    {
+        public Guid CategoryId { get; private set; }
+
+        public AssignCategory(Rule rule) : base(rule) { }
+
+        protected override void Initialize(string actionData)
+        {
+            this.CategoryId = JsonConvert.DeserializeObject<Guid>(actionData);
+        }
+
+        protected override string GetData()
+        {
+            return JsonConvert.SerializeObject(this.CategoryId);
+        }
+
+        public override void Process(TransactionCategoryRow transaction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
